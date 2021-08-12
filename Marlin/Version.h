053d22de7b7c46e7a39f5cd3a33e52c4d55af8bb
Andrea Pirazzini <info@help3d.it>
2021-08-12 11:34:04 +0200

    mod

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "V1_GeniusBL_HELP3D"
+#define SHORT_BUILD_VERSION "V7_GeniusBL_HELP3D"
 
 /**
  * Verbose version identifier which should contain a reference to the location
